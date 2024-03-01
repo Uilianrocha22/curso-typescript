3e590bbc9cdc7e2c671c20ba6767f10ffa6fafb2
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-03-01 20:30:30 -0300

    tuplas no typescript

---
+++ tuplas/dist/main.js
@@ -0,0 +1,4 @@
+let point;
+point = [2, 5, "uilian", true];
+let [x, y, z] = point;
+console.log(point[0]);
--- tuplas/tuplas.ts
+++ tuplas/tuplas.ts
@@ -3,3 +3,5 @@ let point: [number, number, string, boolean];
 point = [2, 5, "uilian", true];
 
 let [x, y, z] = point;
+
+console.log(point[0]);
