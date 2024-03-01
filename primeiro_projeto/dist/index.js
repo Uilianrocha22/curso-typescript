c76da800bfa4790a4557455a689bec9ccb435d7b
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-03-01 20:56:13 -0300

    alteração nos arquivos tsconfig e no tuplas.ts

--- primeiro_projeto/dist/index.js
+++ primeiro_projeto/dist/index.js
@@ -1,26 +1,24 @@
 function sendSpaceShip(name, capitan) {
-  const spaceship = {
-    name,
-    capitan,
-    speed: 20,
-    inMission: true,
-    crew: [],
-  };
-  alert(
-    `A nave ${spaceship.name} comandada pelo capitão ${spaceship.capitan} foi enviada em uma missão`
-  );
-  return spaceship;
+    const spaceship = {
+        name,
+        capitan,
+        speed: 20,
+        inMission: true,
+        crew: [],
+    };
+    alert(`A nave ${spaceship.name} comandada pelo capitão ${spaceship.capitan} foi enviada em uma missão`);
+    return spaceship;
 }
 function accelerate(targetSpeed, spaceship) {
-  if (spaceship.speed > targetSpeed) {
-    alert(`Reduzindo a velocidade da ${spaceship.name} para ${targetSpeed}...`);
-  } else if (spaceship.speed < targetSpeed) {
-    alert(
-      `Almentando a velocidade da ${spaceship.name} para ${targetSpeed}...`
-    );
-  } else {
-    alert(`Mantendo a velocidade da ${spaceship.name} ...`);
-  }
+    if (spaceship.speed > targetSpeed) {
+        alert(`Reduzindo a velocidade da ${spaceship.name} para ${targetSpeed}...`);
+    }
+    else if (spaceship.speed < targetSpeed) {
+        alert(`Almentando a velocidade da ${spaceship.name} para ${targetSpeed}...`);
+    }
+    else {
+        alert(`Mantendo a velocidade da ${spaceship.name} ...`);
+    }
 }
 const spaceshipName = prompt(`Insira o nome da nave a ser enviada.`);
 const spaceshipCapitan = prompt(`Insira o nome do capitão da nave`);
