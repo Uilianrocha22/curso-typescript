01768db70da3d1c345ae4fb796fd116171d25545
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-02-29 22:22:43 -0300

    meu primeiro script em typescript

---
+++ primeiro_projeto/dist/index.js
@@ -0,0 +1,27 @@
+function sendSpaceShip(name, capitan) {
+    const spaceship = {
+        name,
+        capitan,
+        speed: 20,
+        inMission: true,
+        crew: [],
+    };
+    alert(`A nave ${spaceship.name} comandada pelo capitão ${spaceship.capitan} foi enviada em uma missão`);
+    return spaceship;
+}
+function accelerate(targetSpeed, spaceship) {
+    if (spaceship.speed > targetSpeed) {
+        alert(`Reduzindo a velocidade da ${spaceship.name} para ${targetSpeed}...`);
+    }
+    else if (spaceship.speed < targetSpeed) {
+        alert(`Almentando a velocidade da ${spaceship.name} para ${targetSpeed}...`);
+    }
+    else {
+        alert(`Mantendo a velocidade da ${spaceship.name} ...`);
+    }
+}
+const spaceshipName = prompt(`Insira o nome da nave a ser enviada.`);
+const spaceshipCapitan = prompt(`Insira o nome do capitão da nave`);
+const currentShip = sendSpaceShip(spaceshipName, spaceshipCapitan);
+const speed = +prompt(`Insira a velocidade que deseja alterar.`);
+accelerate(speed, currentShip);
---
+++ primeiro_projeto/src/index.ts
@@ -0,0 +1,39 @@
+function sendSpaceShip(name: string, capitan: string) {
+  const spaceship = {
+    name,
+    capitan,
+    speed: 20,
+    inMission: true,
+    crew: [],
+  };
+
+  alert(
+    `A nave ${spaceship.name} comandada pelo capitão ${spaceship.capitan} foi enviada em uma missão`
+  );
+
+  return spaceship;
+}
+
+function accelerate(
+  targetSpeed: number,
+  spaceship: { name: string; speed: number }
+) {
+  if (spaceship.speed > targetSpeed) {
+    alert(`Reduzindo a velocidade da ${spaceship.name} para ${targetSpeed}...`);
+  } else if (spaceship.speed < targetSpeed) {
+    alert(
+      `Almentando a velocidade da ${spaceship.name} para ${targetSpeed}...`
+    );
+  } else {
+    alert(`Mantendo a velocidade da ${spaceship.name} ...`);
+  }
+}
+
+const spaceshipName = prompt(`Insira o nome da nave a ser enviada.`);
+const spaceshipCapitan = prompt(`Insira o nome do capitão da nave`);
+
+const currentShip = sendSpaceShip(spaceshipName, spaceshipCapitan);
+
+const speed = +prompt(`Insira a velocidade que deseja alterar.`);
+
+accelerate(speed, currentShip);
