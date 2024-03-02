d3dc1a9b0dcc61f347352d5d7aad70dae44b377b
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-03-02 20:29:04 -0300

    atualização nos arquivos enum.ts e main.js

--- enums/enum.ts
+++ enums/enum.ts
@@ -1,6 +1,14 @@
-enum planets {
-  MERCURIO,
-  VENUS,
-  TERRA,
-  MARTE,
+enum Planets {
+  MERCURIO = "mercurio",
+  VENUS = "venus",
+  TERRA = "terra",
+  MARTE = "marte",
 }
+
+enum Roles {
+  ADMIN = 0,
+  USER = "uilian",
+}
+
+console.log(Planets.TERRA);
+console.log(Roles.USER);
--- enums/main.js
+++ enums/main.js
@@ -1,7 +1,14 @@
-var planets;
-(function (planets) {
-    planets[planets["MERCURIO"] = 0] = "MERCURIO";
-    planets[planets["VENUS"] = 1] = "VENUS";
-    planets[planets["TERRA"] = 2] = "TERRA";
-    planets[planets["MARTE"] = 3] = "MARTE";
-})(planets || (planets = {}));
+var Planets;
+(function (Planets) {
+    Planets["MERCURIO"] = "mercurio";
+    Planets["VENUS"] = "venus";
+    Planets["TERRA"] = "terra";
+    Planets["MARTE"] = "marte";
+})(Planets || (Planets = {}));
+var Roles;
+(function (Roles) {
+    Roles[Roles["ADMIN"] = 0] = "ADMIN";
+    Roles["USER"] = "uilian";
+})(Roles || (Roles = {}));
+console.log(Planets.TERRA);
+console.log(Roles.USER);
