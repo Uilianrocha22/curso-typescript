d05b379e1dd862ddcb603c72b98e095db0eacfa9
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-03-02 20:58:13 -0300

    aprendendo a evitar tipagem com ?, unknown, any e never

---
+++ evitandoTipagem/evitandoTipagem.ts
@@ -0,0 +1,38 @@
+// Para evitar a tipagem basta colocar um ( ? ) após o parametro
+
+// EX no copilot :
+
+function sendSpaceShip2(spaceship: { pilot: string; copilot?: string }) {
+  //.......................
+}
+
+sendSpaceShip2({ pilot: "uilian", copilot: "chewbacca" });
+sendSpaceShip2({ pilot: "luke" });
+//---------------------------------------------------------------------------------
+
+//let input: unknown;
+let input: any;
+
+input = "teste";
+input = 5;
+input = [];
+
+let text: string;
+
+text = input;
+input = text;
+
+//-----------------------------------------------------------------------------------
+
+function verification(test) {
+  if (test) {
+  } else {
+    let check: never;
+
+    let text = check;
+
+    text = "";
+
+    return check;
+  }
+}
