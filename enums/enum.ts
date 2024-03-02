be836090f1370a934aaec59fad19b3c1b83d6146
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-03-02 20:10:28 -0300

    criação do arquivo enums.ts, tsconfig e compilação para o main.js

---
+++ enums/enum.ts
@@ -0,0 +1,6 @@
+enum planets {
+  MERCURIO,
+  VENUS,
+  TERRA,
+  MARTE,
+}
---
+++ enums/main.js
@@ -0,0 +1,7 @@
+var planets;
+(function (planets) {
+    planets[planets["MERCURIO"] = 0] = "MERCURIO";
+    planets[planets["VENUS"] = 1] = "VENUS";
+    planets[planets["TERRA"] = 2] = "TERRA";
+    planets[planets["MARTE"] = 3] = "MARTE";
+})(planets || (planets = {}));
