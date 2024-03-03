8f55c357a27fc3bb4a9e70fca963d91a502938f4
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-03-02 22:43:21 -0300

    adicionando as funções addSpaceship e findSpaceship

--- exDeclarandoTipos/public/dist/index.js
+++ exDeclarandoTipos/public/dist/index.js
@@ -1,4 +1,23 @@
-function sum(a, b) {
-    return a + b;
+// Um array vai ser usado para salvar as naves de forma mais simplificada
+const spaceships = [];
+// Funções principais
+// Primeiro a função de adicionar uma nave:
+function addSpaceship(name, pilot, crewLimit) {
+    const spaceship = {
+        name,
+        pilot,
+        crewLimit,
+        crew: [],
+        inMission: false,
+    };
+    spaceships.push(spaceship);
+    alert(`A nave ${spaceship.name} foi registrada.`);
 }
-console.log(sum(10, 4));
+//  7. Agora a função de encontrar uma nave:
+function findSpaceship(name) {
+    let spaceship;
+    spaceship = spaceships.find((ship) => {
+        return ship.name === name;
+    });
+    return spaceship;
+}
--- exDeclarandoTipos/public/src/index.ts
+++ exDeclarandoTipos/public/src/index.ts
@@ -1,5 +1,38 @@
-function sum(a: number, b: number) {
-  return a + b;
+// Um array vai ser usado para salvar as naves de forma mais simplificada
+
+const spaceships = [];
+
+// Funções principais
+
+// Primeiro a função de adicionar uma nave:
+function addSpaceship(name: string, pilot: string, crewLimit: number) {
+  const spaceship = {
+    name,
+    pilot,
+    crewLimit,
+    crew: [],
+    inMission: false,
+  };
+
+  spaceships.push(spaceship);
+
+  alert(`A nave ${spaceship.name} foi registrada.`);
 }
 
-console.log(sum(10, 4));
+//  7. Agora a função de encontrar uma nave:
+
+function findSpaceship(name: string) {
+  let spaceship: {
+    name: string;
+    pilot: string;
+    crewLimit: Number;
+    crew: string[];
+    inMission: boolean;
+  };
+
+  spaceship = spaceships.find((ship) => {
+    return ship.name === name;
+  });
+
+  return spaceship;
+}
