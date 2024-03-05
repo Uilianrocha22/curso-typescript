d9afbe530ee81c118d28b31883c687c4b1d8da8f
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-03-05 14:21:21 -0300

    inclussão da função forthMenu e inclussão do Menu de interação

--- exDeclarandoTipos/public/dist/index.js
+++ exDeclarandoTipos/public/dist/index.js
@@ -72,9 +72,60 @@ function thirdMenuOption() {
     const spaceshipName = prompt(`Qual é o nome da nave a ser enviada?`);
     const spaceship = findSpaceship(spaceshipName);
     if (spaceship) {
-        const confirmation = confirm(`Confirma o envio da ${spaceship.name} na missão? `);
+        const confirmation = confirm(`Confirma o envio da ${spaceship.name} na missão ? `);
         if (confirmation) {
             sendInMission(spaceship);
         }
+    }
+}
+// 4° opção
+function fourthMenuOption() {
+    let list = "Naves registradas:";
+    spaceships.forEach((spaceship) => {
+        list += `
+     
+     Nave: ${spaceship.name}
+     Piloto: ${spaceship.pilot}
+     Em missão: ${spaceship.inMission ? "sim" : "não"}
+     Tamanho máximo da tripulação: ${spaceship.crewLimit}
+     Tripulantes: ${spaceship.crew.length}  
+   `;
+        spaceship.crew.forEach((member) => {
+            list += `
+        -${member}`;
+        });
+    });
+    alert(list);
+}
+// Menu :
+let userOption = 0;
+while (userOption !== 5) {
+    const menu = `Painel principal :\n
+     1 - Registrar uma nova nave
+     2 - Adicionar membro da tripulação
+     3 - Enviar nave em missão
+     4 - Listar nave registradas
+     5 - Encerrar
+     `;
+    userOption = Number.parseInt(prompt(menu));
+    switch (userOption) {
+        case 1:
+            firstMenuOption();
+            break;
+        case 2:
+            secondMenuOption();
+            break;
+        case 3:
+            thirdMenuOption();
+            break;
+        case 4:
+            fourthMenuOption();
+            break;
+        case 5:
+            alert(`Encerrando o sistema...`);
+            break;
+        default:
+            alert(`Opção inválida! Retornando ao painel principal...`);
+            break;
     }
 }
--- exDeclarandoTipos/public/src/index.ts
+++ exDeclarandoTipos/public/src/index.ts
@@ -120,11 +120,80 @@ function thirdMenuOption() {
 
   if (spaceship) {
     const confirmation = confirm(
-      `Confirma o envio da ${spaceship.name} na missão? `
+      `Confirma o envio da ${spaceship.name} na missão ? `
     );
 
     if (confirmation) {
       sendInMission(spaceship);
     }
+  }
+}
+
+// 4° opção
+
+function fourthMenuOption() {
+  let list = "Naves registradas:";
+
+  spaceships.forEach(
+    (spaceship: {
+      name: string;
+      pilot: string;
+      crewLimit: number;
+      crew: string[];
+      inMission: boolean;
+    }) => {
+      list += `
+     
+     Nave: ${spaceship.name}
+     Piloto: ${spaceship.pilot}
+     Em missão: ${spaceship.inMission ? "sim" : "não"}
+     Tamanho máximo da tripulação: ${spaceship.crewLimit}
+     Tripulantes: ${spaceship.crew.length}  
+   `;
+
+      spaceship.crew.forEach((member) => {
+        list += `
+        -${member}`;
+      });
+    }
+  );
+
+  alert(list);
+}
+
+// Menu :
+
+let userOption = 0;
+
+while (userOption !== 5) {
+  const menu = `Painel principal :\n
+     1 - Registrar uma nova nave
+     2 - Adicionar membro da tripulação
+     3 - Enviar nave em missão
+     4 - Listar nave registradas
+     5 - Encerrar
+     `;
+
+  userOption = Number.parseInt(prompt(menu));
+
+  switch (userOption) {
+    case 1:
+      firstMenuOption();
+      break;
+    case 2:
+      secondMenuOption();
+      break;
+    case 3:
+      thirdMenuOption();
+      break;
+    case 4:
+      fourthMenuOption();
+      break;
+    case 5:
+      alert(`Encerrando o sistema...`);
+      break;
+    default:
+      alert(`Opção inválida! Retornando ao painel principal...`);
+      break;
   }
 }
